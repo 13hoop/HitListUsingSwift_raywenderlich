0da00836325de75e7c78674d5c5b1d69cf6104dc
YongRen <zhanxin13@gmail.com>
2015-09-03 18:10:28 +0800

    create Hitlist project－Checking the Use Core Data box 	Xcode to generate boilerplate code for what’s known as a Core Data stack in AppDelegate.swift.

---
+++ HitListUsingSwift_raywenderlich/HitListUsingSwift_raywenderlich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitListUsingSwift_raywenderlich
+//
+//  Created by YongRen on 15/9/3.
+//  Copyright (c) 2015年 YongRen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
