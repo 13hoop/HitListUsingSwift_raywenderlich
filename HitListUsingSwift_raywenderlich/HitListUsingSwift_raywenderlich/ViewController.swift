b287d603bc54ca2cb1e60ade4872061049620db1
YongRen <zhanxin13@gmail.com>
2015-09-04 21:15:46 +0800

    基本完成数据固化和读取 注意 1> 凡是操作都首先获取managedContext 2> 建立fetchRequset或者初始化托管对象+借助KVC赋值属性 3> 结果错误处理

--- HitListUsingSwift_raywenderlich/HitListUsingSwift_raywenderlich/ViewController.swift
+++ HitListUsingSwift_raywenderlich/HitListUsingSwift_raywenderlich/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    // 姓名们...
 //    var names = [String]()
     // Change “names” to “people” and [String] to [NSManagedObject]
     // 使用强大的NSManagedObject实例，来替代简单的string
@@ -23,12 +22,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         title = "\"The List\""
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        loadName("Person")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // 添加联系人：借助UIAlertController
@@ -40,15 +41,14 @@ class ViewController: UIViewController {
         //  － 保存
         let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default) { (_) -> Void in
             let textField = alert.textFields![0] as! UITextField
-//            self.names.append(textField.text)
+//          self.names.append(textField.text)
+            // 保存核心方法
             self.saveName(textField.text)
             self.tableView.reloadData()
         }
         //  － 取消
-        // UIAlertActionStyle的3种style
       let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
         }
-//    let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Destructive, handler: nil)
         
         // b 配置
         // - 1 输入框文本的配置
@@ -64,10 +64,56 @@ class ViewController: UIViewController {
     }
     
     /*
-        保存输入的姓名－－－coredata
+        保存/data persistence－－－coredata
     */
     func saveName(name: String) {
         print("保存姓名，不光只是load到cell，更要对应保存到entity中...")
+        
+        // 1 获取对象托管上下文－－xcode自动在AppDelegate中创建
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        // 2 创建被托管的对象，并添加到上下文中－－与创建的database文件对映
+        // An entity description is the piece that links the entity definition from your data model with an instance of NSManagedObject at runtime.
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext!)
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext!)
+
+        // 3 使用KVC向属性（attribute）赋值
+        person.setValue(name, forKey: "name")
+        
+        // 4 错误处理
+        var error: NSError?
+        if !managedContext!.save(&error) {
+            print("保存失败！\(error),\(error?.userInfo)")
+        }
+        
+        // 5 也要添加到‘联系人’数组－－内存中
+        people.append(person)
+    }
+    
+    /*
+        获取/Fetch data－－－coredata
+    */
+    func loadName(entityName: String) {
+        
+        // 1 获取context
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        // 2 创建获取请求，以实体名称搜索
+        let fetchRequest = NSFetchRequest(entityName: entityName)
+        
+        // 3 错误处理
+        var error: NSError?
+        
+        let fetchResults = managedContext?.executeFetchRequest(fetchRequest, error: &error) as? [NSManagedObject]
+        
+        // 正确获取结果，则读取数据到Person，如果错误则输出错误
+        if let results = fetchResults {
+            people = results
+        } else {
+            print("读取数据错误!\(error),\(error!.userInfo)")
+        }
     }
 
 }
@@ -80,7 +126,6 @@ extension ViewController: UITableViewDataSource {
         return people.count
     }
     
-    // dequeues table view cells and populates them with the corresponding string in the names array
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
         
